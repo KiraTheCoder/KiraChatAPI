741c3afbe54eb26d4137f19d8987337dafc674c7
kiran <kiranbohraofficial@gmail.com>
2024-08-28 17:39:41 +0530

    update

--- src/services/utils/socket/socket.ts
+++ src/services/utils/socket/socket.ts
@@ -1,48 +1,54 @@
 import { logger } from "@src/logger";
-import { singleUserSocket } from "@src/services/utils/socket/singleUserSocket"
-import { groupSocket } from "./groupChat"
+import { singleUserSocket } from "@src/services/utils/socket/singleUserSocket";
+import { groupSocket } from "./groupChat";
+import { jwtVerifyToken } from "@src/services/lib/jwt/jwtTokenVerify";
 
 const activeUsers = {};
 
 function socketFn(io) {
-    io.on('connection', (socket) => {
-        
-        socket.onAny((eventName,) => logger.info(`socket incoming event ${JSON.stringify(eventName)}`, { __filename }));
-        socket.onAnyOutgoing((eventName,) => logger.info(`socket out going event ${JSON.stringify(eventName)}`, { __filename }));
+  io.on("connection", (socket) => {
+    socket.onAny((eventName) =>
+      logger.info(`socket incoming event ${JSON.stringify(eventName)}`, {
+        __filename,
+      })
+    );
+    socket.onAnyOutgoing((eventName) =>
+      logger.info(`socket out going event ${JSON.stringify(eventName)}`, {
+        __filename,
+      })
+    );
 
-        const { userId } = (socket as any).userId
-        activeUsers[userId] = socket.id;
+    activeUsers[(socket as any).userId] = socket.id;
 
+    logger.error("id " + (socket as any).userId);
 
-        logger.info("Socket connected succesfully");
-        logger.info(`active users :  ${JSON.stringify(activeUsers)}`)
+    logger.info("Socket connected succesfully");
+    logger.info(`active users :  ${JSON.stringify(activeUsers)}`);
 
-        //////////////
+    //////////////
 
-        // emit all connected user
-        io.emit("welcome", "welcome to Kira Chat App")
+    // emit all connected user
+    io.emit("welcome", "welcome to Kira Chat App");
 
-        // broadcast all except sender
-        socket?.broadcast?.emit(`welcome`, `welcome Hey, ${socket.id} broadcast message`)
+    // broadcast all except sender
+    socket?.broadcast?.emit(
+      `welcome`,
+      `welcome Hey, ${socket.id} broadcast message`
+    );
 
-        // // emit only  which current client
-        socket.emit("my_socket_id", socket.id)
+    // // emit only  which current client
+    socket.emit("my_socket_id", socket.id);
 
-        /////////  single user chatting /////////////
-        singleUserSocket(io, socket, activeUsers, userId);
+    /////////  single user chatting /////////////
+    // singleUserSocket(io, socket, activeUsers, userId);
 
+    /////////   group chatting /////////////
+    // groupSocket(io, socket, activeUsers, userId);
 
-        /////////   group chatting /////////////
-        groupSocket(io, socket, activeUsers, userId);
-
-
-        socket.on('disconnect', () => {
-            delete activeUsers[socket.userId];
-        });
-
-
+    socket.on("disconnect", () => {
+      delete activeUsers[socket.userId];
     });
+  });
 }
-
 
 export { socketFn };
