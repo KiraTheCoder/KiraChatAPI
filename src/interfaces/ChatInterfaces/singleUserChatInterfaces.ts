7742d08bf88c8cb2ad13aa8ce6d3d285ee3c6ec0
kiran <kiranbohraofficial@gmail.com>
2024-09-10 12:38:27 +0530

    remove _id

--- src/interfaces/ChatInterfaces/singleUserChatInterfaces.ts
+++ src/interfaces/ChatInterfaces/singleUserChatInterfaces.ts
@@ -1,4 +1,5 @@
 export interface IsingleUserChat {
+    _id:boolean
     chatId: [any],
     messages: { userId: string, message: string, createdAt: string }[],
 }
--- src/models/chatModels/singleUserChatModel.ts
+++ src/models/chatModels/singleUserChatModel.ts
@@ -2,6 +2,7 @@ import { model, Schema, Types } from "mongoose";
 import { IsingleUserChat } from "@src/interfaces";
 
 const singleUserChatSchema = new Schema<IsingleUserChat>({
+    _id:false,
     chatId: {
         type: [{ type: Types.ObjectId, ref: 'users', required: true }],
         required: true,
